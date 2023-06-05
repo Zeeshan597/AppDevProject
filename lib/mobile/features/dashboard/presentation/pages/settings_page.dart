5b1d728e3e2a1f6cbc590a60c4c7eceb7c8afd66
Zeeshan597 <zeeshanahamed597@gmail.com>
2023-06-05 22:19:33 +0500

    AppDev

--- lib/mobile/features/dashboard/presentation/pages/settings_page.dart
+++ lib/mobile/features/dashboard/presentation/pages/settings_page.dart
@@ -6,8 +6,10 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:provider/provider.dart';
-
 import '../../../../../core/theme/theme_provider.dart';
+import 'about_us.dart';
+import 'terms_and_conditions.dart';
+import 'privacy_policy.dart';
 
 @RoutePage()
 class SettingsPage extends StatelessWidget {
@@ -20,53 +22,62 @@ class SettingsPage extends StatelessWidget {
         return Scaffold(
           body: ListView(
             padding: const EdgeInsets.all(16.0),
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('Settings Page', style: context.titleMedium),
-              ),
-              ListTile(
-                leading: const Icon(LineIcons.fileAlt),
-                title: const Text('Terms and conditions'),
-                onTap: () {},
-                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
-              ),
-              ListTile(
-                leading: const Icon(LineIcons.userShield),
-                title: const Text('Privacy policy'),
-                onTap: () {},
-                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
-              ),
-              ListTile(
-                leading: const Icon(LineIcons.headset),
-                title: const Text('Help Center'),
-                onTap: () {},
-                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
-              ),
-              ListTile(
-                leading: const Icon(LineIcons.infoCircle),
-                title: const Text('About us'),
-                onTap: () {},
-                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
-              ),
-              ListTile(
-                leading: const Icon(LineIcons.alternateSignOut),
-                title: const Text(AppStrings.logout),
-                onTap: () {
-                  context.read<UserCubit>().logout().then((_) {
-                    context.router.replaceAll([const WelcomeRoute()]);
-                  });
+                    children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Additional Information', style: context.titleMedium),
+          ),
+          ListTile(
+            leading: const Icon(LineIcons.fileAlt),
+            title: const Text('Terms and conditions'),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const TermsAndConditions();
                 },
-                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
-              ),
-              SwitchListTile(
-                title: const Text('Dark Mode'),
-                value: themeProvider.isDarkMode,
-                onChanged: (value) {
-                  themeProvider.toggleTheme();
+              ));
+            },
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0))),
+          ),
+          ListTile(
+            leading: const Icon(LineIcons.userShield),
+            title: const Text('Privacy policy'),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const PrivacyPolicy();
                 },
-              ),
-            ],
+              ));
+            },
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0))),
+          ),
+          ListTile(
+            leading: const Icon(LineIcons.infoCircle),
+            title: const Text('About us'),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const AboutUs();
+                },
+              ));
+            },
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0))),
+          ),
+          ListTile(
+            leading: const Icon(LineIcons.alternateSignOut),
+            title: const Text(AppStrings.logout),
+            onTap: () {
+              context.read<UserCubit>().logout().then((_) {
+                context.router.replaceAll([const WelcomeRoute()]);
+              });
+            },
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0))),
+          ),
+        ],
           ),
         );
       },
